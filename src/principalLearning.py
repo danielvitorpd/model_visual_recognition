453962949c5aa88edcc15b92b22409906de4f279
zuspat <danielvitor@usp.br>
2020-05-09 19:28:22 -0300

    put machine learning :ok:

---
+++ src/principalLearning.py
@@ -0,0 +1,81 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.metrics import accuracy_score
+import pickle
+from joblib import dump, load
+from sklearn import svm
+from sklearn import datasets
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, Dropout
+from keras.utils import to_categorical
+
+train = pd.read_csv('relatorio.csv')
+test = pd.read_csv('teste.csv')
+
+train = np.random.shuffle(train)
+labels = train['label'].values
+unique_val = np.array(labels)
+np.unique(unique_val)
+
+train.drop('label', axis = 1, inplace = True)
+images = train.values
+images = np.array([np.reshape(i, (28, 28)) for i in images])
+images = np.array([i.flatten() for i in images])
+
+label_binrizer = LabelBinarizer()
+labels = label_binrizer.fit_transform(labels)
+
+x_train, x_test, y_train, y_test = train_test_split(images, labels, test_size = 0.3, random_state = 101)
+
+print(x_train.shape)
+print(x_test.shape)
+
+callbacks = [keras.callbacks.EarlyStopping(monitor='val_loss')]
+
+batch_size = 128
+num_classes = 2
+epochs = 30
+x_train = x_train / 255
+x_test = x_test / 255
+x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+plt.imshow(x_train[0].reshape(28,28))
+
+model = Sequential()
+model.add(Conv2D(64, kernel_size=(3,3), activation = 'relu', input_shape=(28, 28 ,1)))
+model.add(MaxPooling2D(pool_size = (2, 2)))
+
+model.add(Conv2D(64, kernel_size = (3, 3), activation = 'relu'))
+model.add(MaxPooling2D(pool_size = (2, 2)))
+
+model.add(Flatten())
+model.add(Dense(32, activation = 'relu'))
+model.add(Dropout(0.20))
+
+model.add(Dense(num_classes, activation = 'softmax'))
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+history = model.fit(x_train, y_train, epochs=epochs, batch_size=480, callbacks=callbacks)
+
+filename = 'predict_digits_model.sav'
+# salva o modelo no disco
+pickle.dump(model, open(filename, 'wb'))
+dump(model,"machinelearning.joblib")
+
+test_labels = test['label']
+test.drop('label', axis = 1, inplace = True)
+test_images = test.values
+test_images = np.array([np.reshape(i, (28, 28)) for i in test_images])
+test_images = np.array([i.flatten() for i in test_images])
+test_labels = label_binrizer.fit_transform(test_labels)
+test_images = test_images.reshape(test_images.shape[0], 28, 28, 1)
+tests = model.predict(test_images)
+for predict in range(13):
+  print('resultado teste:', np.argmax(tests[predict]))
+  print('número da imagem de teste:', test_labels[predict])
